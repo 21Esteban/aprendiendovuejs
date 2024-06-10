e76b5c0a48cc39390d25416169f469326a7e23d6
21Esteban <jelc200521@gmail.com>
2024-06-09 19:54:35 -0500

    como usar componentes en VueJS

---
+++ Componentes/src/App.vue
@@ -0,0 +1,47 @@
+<!-- Crear un Componente
+Primero, debes definir un componente. Puedes hacerlo en el mismo archivo .vue o en un archivo separado. Aquí tienes un ejemplo de un componente llamado MyComponent -->
+
+
+<!-- 2. Registrar el Componente
+Para usar el componente padre en otro componente o en el componente raíz (App.vue), debes registrarlo.
+
+En el componente raíz (App.vue): -->
+
+<!-- 3. Usar el Componente
+Una vez que has registrado el componente, puedes usarlo en el template del componente donde lo registraste.
+
+4. Pasar Props y Emitir Eventos
+Para hacer los componentes más dinámicos, puedes pasar datos a través de props y emitir eventos para comunicarse con otros componentes.
+
+Pasar Props:
+
+En padre.vue, define las props que el componente puede recibir: -->
+
+
+<template>
+  <div>
+    <h1>este es el archivo app.vue</h1>
+    <padre :saludo="'Saludos desde un componente usando props' "/>
+  </div>
+</template>
+
+<script>
+import padre from './components/padre.vue'
+
+
+
+export default {
+    data(){
+        return {
+
+        }
+    },
+    components:{
+        padre,
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Componentes/src/components/padre.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="p-5 bg-dark text-white">
+    <h2>{{ saludo }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "ComponentePadre",
+    props:{
+        saludo:{
+            type:String,
+            required:true,
+        }
+    },
+    
+    data(){
+        return {
+            saludo:"Saludos desde el componente padre"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
